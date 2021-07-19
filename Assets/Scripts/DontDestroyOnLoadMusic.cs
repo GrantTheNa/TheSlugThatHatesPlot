4715e0a5b4b42736347f0a746ea09db1cdc6de29
GrantTheNa <kade.grant.richards@gmail.com>
2021-07-19 14:01:26 +1000

    Initial Design
    + Music stays playing during reloads

---
+++ Assets/Scripts/DontDestroyOnLoadMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoadMusic : MonoBehaviour
+{
+    private static DontDestroyOnLoadMusic instance;
+
+ 
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+
+
+
+}
